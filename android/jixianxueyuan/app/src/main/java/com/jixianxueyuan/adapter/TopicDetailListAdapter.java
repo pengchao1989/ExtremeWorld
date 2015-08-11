73e457750f596a71e58489a6d8db32d1fa548516
扳手扳死你 <chaoyue2010@yeah.net>
2015-08-11 23:48:14 +0800

    话题详情页面显示子回复

--- android/jixianxueyuan/app/src/main/java/com/jixianxueyuan/adapter/TopicDetailListAdapter.java
+++ android/jixianxueyuan/app/src/main/java/com/jixianxueyuan/adapter/TopicDetailListAdapter.java
@@ -1,18 +1,29 @@
 package com.jixianxueyuan.adapter;
 
 import android.content.Context;
+import android.graphics.Color;
+import android.text.SpannableString;
+import android.text.Spanned;
+import android.text.TextPaint;
+import android.text.style.ClickableSpan;
+import android.text.style.ForegroundColorSpan;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.jixianxueyuan.R;
 import com.jixianxueyuan.dto.ReplyDTO;
+import com.jixianxueyuan.dto.SubReplyDTO;
 import com.jixianxueyuan.server.StaticResourceConfig;
+import com.jixianxueyuan.util.MyLog;
 import com.nostra13.universalimageloader.core.ImageLoader;
 import com.yumfee.emoji.EmojiconTextView;
+
+import org.w3c.dom.Text;
 
 import java.util.LinkedList;
 import java.util.List;
@@ -94,7 +105,99 @@ public class TopicDetailListAdapter extends BaseAdapter {
         String url = replyDTO.getUser().getAvatar() + "!androidListAvatar";
         ImageLoader.getInstance().displayImage(url, viewHolder.avatarImageView);
 
+        //sub reply
+        showSubReply(viewHolder, replyDTO);
+
         return convertView;
+    }
+
+    private void showSubReply(ViewHolder viewHolder, ReplyDTO replyDTO) {
+        viewHolder.subReplyContent1.setVisibility(View.GONE);
+        viewHolder.subReplyContent2.setVisibility(View.GONE);
+        viewHolder.subReplyContent3.setVisibility(View.GONE);
+        viewHolder.loadMoreSubReplyTextView.setVisibility(View.GONE);
+
+        List<SubReplyDTO> subReplyDTOList = replyDTO.getSubReplys();
+        if(subReplyDTOList.size() > 0){
+            viewHolder.subReplyContent1.setVisibility(View.VISIBLE);
+
+            String name = subReplyDTOList.get(0).getUser().getName();
+            String content = subReplyDTOList.get(0).getContent();
+            SpannableString ss=new SpannableString(name+":"+content);
+
+            //ss.setSpan(new ForegroundColorSpan(Color.BLUE), 0, name.length(), Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+            ss.setSpan(new ClickableSpan(){
+
+                @Override
+                public void updateDrawState(TextPaint ds) {
+                    super.updateDrawState(ds);
+                    ds.setColor(context.getResources().getColor(R.color.blue));       //设置文件颜色
+                    ds.setUnderlineText(true);      //设置下划线
+                }
+
+                @Override
+                public void onClick(View widget) {
+                    Toast.makeText(context, "click", Toast.LENGTH_LONG).show();
+                    MyLog.d("TopicDetailListAdapter", "onCLick");
+                }
+            },0, name.length(), Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+            viewHolder.subReplyContent1.setText(ss);
+
+        }
+        if(subReplyDTOList.size() > 1){
+            viewHolder.subReplyContent2.setVisibility(View.VISIBLE);
+            String name = subReplyDTOList.get(1).getUser().getName();
+            String content = subReplyDTOList.get(1).getContent();
+            SpannableString ss=new SpannableString(name+":"+content);
+
+            //ss.setSpan(new ForegroundColorSpan(Color.BLUE), 0, name.length(), Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+            ss.setSpan(new ClickableSpan() {
+
+                @Override
+                public void updateDrawState(TextPaint ds) {
+                    super.updateDrawState(ds);
+                    ds.setColor(context.getResources().getColor(R.color.blue));       //设置文件颜色
+                    ds.setUnderlineText(true);      //设置下划线
+                }
+
+                @Override
+                public void onClick(View widget) {
+                    Toast.makeText(context, "click", Toast.LENGTH_LONG).show();
+                }
+            }, 0, name.length(), Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+            viewHolder.subReplyContent2.setText(ss);
+        }
+        if(subReplyDTOList.size() > 2){
+            viewHolder.subReplyContent3.setVisibility(View.VISIBLE);
+            String name = subReplyDTOList.get(2).getUser().getName();
+            String content = subReplyDTOList.get(2).getContent();
+            SpannableString ss=new SpannableString(name+":"+content);
+
+            //ss.setSpan(new ForegroundColorSpan(Color.BLUE), 0, name.length(), Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+            ss.setSpan(new ClickableSpan() {
+
+                @Override
+                public void updateDrawState(TextPaint ds) {
+                    super.updateDrawState(ds);
+                    ds.setColor(context.getResources().getColor(R.color.blue));       //设置文件颜色
+                    ds.setUnderlineText(true);      //设置下划线
+                }
+
+                @Override
+                public void onClick(View widget) {
+                    Toast.makeText(context, "click", Toast.LENGTH_LONG).show();
+                }
+            }, 0, name.length(), Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+            viewHolder.subReplyContent3.setText(ss);
+
+
+        }
+        if(subReplyDTOList.size() > 3){
+            viewHolder.loadMoreSubReplyTextView.setVisibility(View.VISIBLE);
+            String loadMoreStr = context.getString(R.string.load_sub_reply);
+            loadMoreStr = String.format(loadMoreStr, subReplyDTOList.size() - 3);
+            viewHolder.loadMoreSubReplyTextView.setText(loadMoreStr);
+        }
     }
 
 
@@ -104,6 +207,15 @@ public class TopicDetailListAdapter extends BaseAdapter {
         @InjectView(R.id.user_head_name)TextView nameTextView;
         @InjectView(R.id.user_head_time)TextView timeTextView;
         @InjectView(R.id.topic_detail_list_item_reply_content)EmojiconTextView replyContentTextView;
+
+        @InjectView(R.id.topic_detail_list_item_sub_reply_content_1)
+        EmojiconTextView subReplyContent1;
+        @InjectView(R.id.topic_detail_list_item_sub_reply_content_2)
+        EmojiconTextView subReplyContent2;
+        @InjectView(R.id.topic_detail_list_item_sub_reply_content_3)
+        EmojiconTextView subReplyContent3;
+        @InjectView(R.id.topic_detail_list_item_sub_reply_loadmore)
+        TextView loadMoreSubReplyTextView;
 
         public ViewHolder(View itemView)
         {
