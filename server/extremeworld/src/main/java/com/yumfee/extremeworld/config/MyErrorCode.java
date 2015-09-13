a80bca6b30c3fa844c8638ce33ba810b6a44b2cc
扳手扳死你 <chaoyue2010@yeah.net>
2015-09-13 14:17:29 +0800

    sms verifition code restful of check code

--- server/extremeworld/src/main/java/com/yumfee/extremeworld/config/MyErrorCode.java
+++ server/extremeworld/src/main/java/com/yumfee/extremeworld/config/MyErrorCode.java
@@ -3,7 +3,10 @@ package com.yumfee.extremeworld.config;
 public enum MyErrorCode {
 	
 	NAME_REPEAT(10001, "nick name repeat"), //昵称重复
-	NAME_EMPTY(10002, "nick name empty");
+	NAME_EMPTY(10002, "nick name empty"),
+	PHONE_EMPTY(10003, "phone empty"),
+	SMS_VERIFICATION_CODE_ERROR(10004,"send sms code error"),
+	UNKNOW_ERROR(1001,"unknow error");
 	
 	private int errorCode;
 	private String errorInfo;
