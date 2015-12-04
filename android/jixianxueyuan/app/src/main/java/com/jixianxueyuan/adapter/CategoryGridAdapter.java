72a7981230cf6fa3b1e6f5c328d50b421bfbfb56
扳手扳死你 <chaoyue2010@yeah.net>
2015-12-05 00:27:24 +0800

    优化集市首页分类图片显示
    使用acache缓存7天，显示速度大大加快

--- android/jixianxueyuan/app/src/main/java/com/jixianxueyuan/adapter/CategoryGridAdapter.java
+++ android/jixianxueyuan/app/src/main/java/com/jixianxueyuan/adapter/CategoryGridAdapter.java
@@ -1,6 +1,7 @@
 package com.jixianxueyuan.adapter;
 
 import android.content.Context;
+import android.graphics.Bitmap;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -9,9 +10,13 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.jixianxueyuan.R;
+import com.jixianxueyuan.app.MyApplication;
 import com.jixianxueyuan.config.ImageLoaderConfig;
 import com.jixianxueyuan.dto.biz.CategoryDTO;
+import com.jixianxueyuan.util.ACache;
 import com.nostra13.universalimageloader.core.ImageLoader;
+import com.nostra13.universalimageloader.core.assist.FailReason;
+import com.nostra13.universalimageloader.core.listener.ImageLoadingListener;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -64,10 +69,42 @@ public class CategoryGridAdapter extends BaseAdapter{
             viewHolder = (ViewHolder) convertView.getTag();
         }
 
-        CategoryDTO categoryDTO = categoryDTOList.get(position);
+        final CategoryDTO categoryDTO = categoryDTOList.get(position);
         viewHolder.nameTextView.setText(categoryDTO.getName());
-        ImageLoader imageLoader = ImageLoader.getInstance();
-        imageLoader.displayImage(categoryDTO.getIcon(), viewHolder.iconImageView, ImageLoaderConfig.getImageOption(context));
+        final ACache mCache = ACache.get(context);
+        Bitmap bitmap = mCache.getAsBitmap(categoryDTO.getIcon());
+        if(bitmap != null){
+            viewHolder.iconImageView.setImageBitmap(bitmap);
+        }else {
+            ImageLoader imageLoader = ImageLoader.getInstance();
+            final ViewHolder finalViewHolder = viewHolder;
+            imageLoader.loadImage(categoryDTO.getIcon(), ImageLoaderConfig.getImageOption(context), new ImageLoadingListener() {
+                @Override
+                public void onLoadingStarted(String imageUri, View view) {
+
+                }
+
+                @Override
+                public void onLoadingFailed(String imageUri, View view, FailReason failReason) {
+
+                }
+
+                @Override
+                public void onLoadingComplete(String imageUri, View view, Bitmap loadedImage) {
+                    finalViewHolder.iconImageView.setImageBitmap(loadedImage);
+                    mCache.put(categoryDTO.getIcon(), loadedImage, ACache.TIME_DAY * 7);
+                }
+
+                @Override
+                public void onLoadingCancelled(String imageUri, View view) {
+
+                }
+            });
+        }
+
+
+
+        //imageLoader.displayImage(categoryDTO.getIcon(), viewHolder.iconImageView, ImageLoaderConfig.getImageOption(context));
 
         return convertView;
     }
