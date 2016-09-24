0517ea1157426f2c48b3fbe289ac98b59a1a50ba
pengchao <chaoyue2010@yeah.net>
2016-09-25 00:10:01 +0800

    newbility spalsh activity

---
+++ android/jixianxueyuan/app/src/main/java/com/jixianxueyuan/activity/SplashActivity.java
@@ -0,0 +1,22 @@
+package com.jixianxueyuan.activity;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+import com.jixianxueyuan.MainActivity;
+import com.jixianxueyuan.R;
+
+/**
+ * Created by pengchao on 16-9-24.
+ */
+
+public class SplashActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash_activity);
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+}
